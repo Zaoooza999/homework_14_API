39c05ebb831b5de2bb06eb1014b860087201c32a
mn.ustiuzhanin <zaoooza33@gmail.com>
2026-09-09 21:40:04 +0700

    Complete homework

---
+++ src/test/java/tests/StatusTests.java
@@ -0,0 +1,117 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+public class StatusTests {
+    /*
+    1. Make request to https://selenoid.qa.guru/ui/status
+    2. Get response "state": {
+        "total": 25,
+        "used": 0,
+        "queued": 0,
+        "pending": 0,
+        "warmReady": 4,
+        "warmTotal": 4,
+        "hotReady": 7,
+        "hotTotal": 8,
+        "warmSlots": }
+    3. Check total is 25
+     */
+    @Test
+    public void totalAmountTest() {
+        get("https://selenoid.qa.guru/ui/status")
+                .then()
+                .body("state.total", is(25));
+    }
+    @Test
+    public void totalAmountTest_withLogs() {
+        get("https://selenoid.qa.guru/ui/status")
+                .then()
+                .log().all()
+                .body("state.total", is(25));
+    }
+
+    @Test
+    public void totalAmountTest_withAllLogs() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.qa.guru/ui/status")
+                .then()
+                .log().all()
+                .body("state.total", is(25));
+    }
+    @Test
+    public void status200Test() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.qa.guru/ui/status")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+    @Test
+    public void containingItemsTest() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.qa.guru/ui/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("state",allOf(hasKey("total"),hasKey("used"),hasKey("queued"),hasKey("pending"),
+                        hasKey("warmReady"),hasKey("warmTotal"),hasKey("hotReady"),hasKey("hotTotal"),
+                        hasKey("warmSlots")))
+                .body("state.warmSlots[0]",hasKey("browser"))
+                .body("state.warmSlots.browser",hasItem("chrome"))
+                .body("state.warmSlots.browser",hasItems("chrome","chromium"))
+                .body("browsers",hasKey("android"))
+                .body("browsers",allOf(hasKey("chrome"),hasKey("firefox")))
+                .body("browsers.android",is(0))
+                .body("browsers",hasEntry("firefox",0))
+                .body("browsers",allOf(hasEntry("chrome",0),hasEntry("firefox",0)));
+    }
+
+    @Test
+    public void statusSchemaTest() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.qa.guru/ui/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas\\status_response_schema.json"));
+    }
+
+    @Test
+    public void bestTotalAmountTest() {
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.qa.guru/ui/status")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas\\status_response_schema.json"))
+                .body("state.total", is(25))
+                .body("state",allOf(hasKey("total"),hasKey("used"),hasKey("queued"),hasKey("pending"),
+                        hasKey("warmReady"),hasKey("warmTotal"),hasKey("hotReady"),hasKey("hotTotal"),
+                        hasKey("warmSlots")))
+                .body("state.warmSlots[0]",hasKey("browser"))
+                .body("state.warmSlots.browser",hasItem("chrome"))
+                .body("state.warmSlots.browser",hasItems("chrome","chromium"))
+                .body("browsers",hasKey("android"))
+                .body("browsers",allOf(hasKey("chrome"),hasKey("firefox")))
+                .body("browsers.android",is(0))
+                .body("browsers",hasEntry("firefox",0))
+                .body("browsers",allOf(hasEntry("chrome",0),hasEntry("firefox",0)));
+    }
+}
+
---
+++ src/test/java/tests/WdHubStatusTests.java
@@ -0,0 +1,80 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+public class WdHubStatusTests extends TestBase{
+    @Test
+    public void authorisationTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void readyIsTrueTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .body("value.ready", is(true));
+    }
+
+    @Test
+    public void messageTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .body("value.message", startsWith("Selenoid"));
+    }
+
+    @Test
+    public void statusSchemaTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "1234")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .body(matchesJsonSchemaInClasspath("schemas/wd_hub_status_response_schema.json"));
+    }
+
+    @Test
+    public void unauthorizedStatusTest() {
+        given()
+                .log().all()
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(401);
+    }
+
+    @Test
+    public void incorrectedPasswordStatusTest() {
+        given()
+                .log().all()
+                .auth().basic("user1", "12345")
+                .when()
+                .get("/wd/hub/status")
+                .then()
+                .log().all()
+                .statusCode(401);
+    }
+}
